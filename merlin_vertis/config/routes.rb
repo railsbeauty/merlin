32838af5498e6bbbaa02b4e4737ff5bc7bb63e8b
Vinay <vinay59.grewal@gmail.com>
2013-05-14 03:55:10 +0530

    added username to user table, home controller

--- merlin_vertis/config/routes.rb
+++ merlin_vertis/config/routes.rb
@@ -1,5 +1,9 @@
 MerlinVertis::Application.routes.draw do
+  
+
   devise_for :users
+  root :to => "home/index"
+  resources :home
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
