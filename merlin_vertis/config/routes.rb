aa3fe9a5983659a8478cb78078f898e4ce24a2e1
vinay <vinay@vertisinfotech.com>
2013-05-14 12:15:47 +0530

    remove faulty home controller

--- merlin_vertis/config/routes.rb
+++ merlin_vertis/config/routes.rb
@@ -2,8 +2,7 @@ MerlinVertis::Application.routes.draw do
   
 
   devise_for :users
-  root :to => "home/index"
-  resources :home
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
